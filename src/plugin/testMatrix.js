2c9f2f66919bd60ff489555d8978be67c6a4b3cb
jlylb <jlylb@126.com>
2019-08-12 00:11:53 +0800

    add test particle

--- src/plugin/testMatrix.js
+++ src/plugin/testMatrix.js
@@ -1,16 +1,20 @@
 const particleVertexShader = [
+  'attribute float size;',
+  'uniform float speed;',
   'void main()',
   '{',
-  'vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
-
+  'vec4 mvPosition = modelViewMatrix * vec4( position + vec3(0.0, speed, 0.0), 1.0 );',
+  'gl_PointSize =  size * ( 300.0 / -mvPosition.z );',
   'gl_Position = projectionMatrix * mvPosition;',
   '}'
 ].join('\n')
 
 const particleFragmentShader = [
+  'uniform sampler2D pointTexture;',
   'void main()',
   '{',
-  'gl_FragColor = vec4( 0.0, 1.0, 0.0, 1.0 );',
+  'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+  'gl_FragColor = gl_FragColor * texture2D( pointTexture, gl_PointCoord );',
   '}'
 ].join('\n')
 
--- src/views/c25.vue
+++ src/views/c25.vue
@@ -13,6 +13,14 @@ const TWEEN = require("@tweenjs/tween.js");
 
 import testM from "@/plugin/testMatrix.js";
 
+var smokePng = require('../assets/patricle/smokeparticle.png')
+
+let geo
+let boxMesh
+let dt = 0.0
+
+let clock = new THREE.Clock();
+
 export default {
   components: {},
   data() {
@@ -37,7 +45,7 @@ export default {
       //   camera.position.z = 150;
       //   camera.position.y = 0;
       //   camera.position.x = 0;
-      camera.position.set(0, 400, 400);
+      camera.position.set(0, 0, 300);
       //   camera.up.x = 0;
       //   camera.up.y = 1;
       //   camera.up.z = 0;
@@ -72,22 +80,79 @@ export default {
     },
 
     initObjects() {
-      const box = new THREE.BoxGeometry(100, 100, 100);
+      this.createParticle()
+    },
+    createParticle() {
+      geo = new THREE.BufferGeometry()
+      const vertices = 500
+      const radius = 50
 
-      const boxMaterial = new THREE.ShaderMaterial(testM);
+      const positions = []
+      const sizes = [];
 
-      const boxMesh = new THREE.Mesh(box, boxMaterial);
+      	for ( var i = 0; i < vertices; i ++ ) {
+					positions.push( ( Math.random() * 2 - 1 ) * radius );
+					positions.push( ( Math.random() * 2 - 1 ) * radius );
+					positions.push( ( Math.random() * 2 - 1 ) * radius );
+          sizes.push( 20 );
+         
+        }
+        
+        geo.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+         geo.addAttribute( 'size', new THREE.Float32BufferAttribute( sizes, 1 ).setDynamic( true ) );
 
-      scene.add(boxMesh);
+        const uniforms = {
+          pointTexture: { value: new THREE.TextureLoader().load( smokePng ) },
+          speed: { value: 0.05 }
+        };
+        
+
+        const boxMaterial = new THREE.ShaderMaterial({
+          uniforms: uniforms,
+					vertexShader: testM.vertexShader,
+					fragmentShader: testM.fragmentShader,
+					blending: THREE.AdditiveBlending,
+					depthTest: false,
+					transparent: true,
+					vertexColors: true
+        });
+
+        boxMesh = new THREE.Points(geo, boxMaterial);
+console.log(boxMesh)
+        scene.add(boxMesh);
+
+    },
+
+    animation() {
+     // var time = Date.now() * 0.005;
+     // let dt = clock.getDelta()
+     // boxMesh.rotation.z += 0.1*dt;
+      				var sizes = geo.attributes.size.array;
+				for ( var i = 0; i < 500; i ++ ) {
+					sizes[ i ] = 10 * ( 1 + Math.sin( 0.1 * i + dt ) );
+				}
+        geo.attributes.size.needsUpdate = true;
+
+        dt += 0.01
+        if(dt>0.5) {
+          dt = 0.0
+        }
+        boxMesh.material.uniforms.speed.value = dt * 200
+
+        // boxMesh.material.needsUpdate = true
     },
 
     render() {
       if (TWEEN != null && typeof TWEEN != "undefined") {
         TWEEN.update();
       }
+       requestAnimationFrame(this.render);
+ this.animation()
 
       renderer.render(scene, camera);
-      requestAnimationFrame(this.render);
+     
+
+     
     }
   },
   mounted() {
